99b4dad0c39bd1b634d3afad01fdd4bbe0afec8e
Hieu Rocker <rockerhieu@gmail.com>
2016-04-07 00:23:51 +0700

    Add initial Parse CloudCode

---
+++ Backend/cloud/main.js
@@ -0,0 +1,6 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hellothere", function(request, response) {
+  response.success("Hello world!");
+});
