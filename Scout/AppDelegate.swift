bc68435caab072aa99ab1a0da44e6dcfbc0943df
buddybuild <help@buddybuild.com>
2016-04-06 09:52:42 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Scout/AppDelegate.swift
+++ Scout/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    BuddyBuildSDK.setup()
+    
     // Override point for customization after application launch.
     return true
   }
