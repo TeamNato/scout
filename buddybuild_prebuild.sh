3f51ac18223d097261f91146e9fd4093d7034658
John Pham <hprobotic@gmail.com>
2016-04-08 14:45:22 +0700

    Update prebuild.sh

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -3,10 +3,9 @@
 echo "<?xml version='1.0' encoding='UTF-8'?>
 <!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
 <plist version='1.0'>
-<dict>
     <key>TestKey</key>
     <string>$TEST_KEY</string>
     <key>GMSApiKey</key>
-    <string>\$(GMSApiKey)</string>
+    <string>$GMS_API_KEY</string>
 </dict>
 </plist>" > Scout/PrivateConfig.plist
