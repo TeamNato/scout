b29a3af1695f739b22fad3d313251678843d1340
Hieu Rocker <rockerhieu@gmail.com>
2016-04-10 00:03:45 +0700

    Prevent Pods project from being overriding by `pod install` on Buddybuild

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -12,3 +12,7 @@ echo "<?xml version='1.0' encoding='UTF-8'?>
     <string>$GMS_API_KEY</string>
 </dict>
 </plist>" > Scout/PrivateConfig.plist
+
+# This file will be reset by `pod install`. We need to reset it to keep the extra
+# settings we've added
+git reset HEAD Pods/Pods.xcodeproj/project.pbxproj
