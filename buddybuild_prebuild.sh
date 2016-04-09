9dd50ec4ed65bb26a446a05581abb012838b878b
redphx <redphoenix89@gmail.com>
2016-04-09 17:10:14 +0700

    Update missing Parse's keys in buddybuild_prebuild.sh

--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -3,9 +3,11 @@
 echo "<?xml version='1.0' encoding='UTF-8'?>
 <!DOCTYPE plist PUBLIC '-//Apple//DTD PLIST 1.0//EN' 'http://www.apple.com/DTDs/PropertyList-1.0.dtd'>
 <plist version='1.0'>
-<dict>    
-    <key>TestKey</key>
-    <string>$TEST_KEY</string>
+<dict>
+    <key>ParseApplicationId</key>
+    <string>$PARSE_APPLICATION_ID</string>
+    <key>ParseClientId</key>
+    <string>$PARSE_CLIENT_ID</string>
     <key>GMSApiKey</key>
     <string>$GMS_API_KEY</string>
 </dict>
